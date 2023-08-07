97e1b035227130250207f882a9948c9976a36b70
Leesumin <Leesumin@gamil.com>
2023-08-07 21:14:23 +0900

    회원가입 기능구현 중(DB 저장 완료)

---
+++ src/main/front/book-view/src/component/Layout/LoginBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './LoginBtn.module.css'
+
+const LoginBtn = () =>{
+    return (
+        <div className = {styles.container} >
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default LoginBtn;
